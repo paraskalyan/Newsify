7ba5278dd914013d5bb8952346414831c7a4dad8
Paras Kalyan <98208059+paraskalyan@users.noreply.github.com>
2023-09-21 18:02:14 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='flex justify-between h-[7vh] items-center px-4 bg-black text-white font-mono'>
+      <div className='font-bold text-[1.5rem]'>Newsify</div>
+      <div className='flex gap-4'>
         <Link to="/">Home</Link>
         <Link to="/login">Log in</Link>
-        <Link to="/register">Register</Link>
+        <Link to="/register">Join us</Link>
+      </div>
     </div>
   )
 }
